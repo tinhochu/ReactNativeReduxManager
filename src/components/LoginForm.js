4b7d60ad2e02ca1d4a3dc63134133d8c910572d4
Tin Chu <mchu.tinho@gmail.com>
2017-11-20 11:07:59 -0600

    separated types from actions to call the reducers

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
 		this.props.emailChanged(text);
+	}
+
+	onPasswordChange(text){
+		this.props.passwordChanged(text);
 	}
 
 	render() {
@@ -17,6 +21,7 @@ class LoginForm extends Component {
 						label={'Email'}
 						placeholder={'your@email.com'}
 						onChangeText={this.onEmailChange.bind(this)}
+						value={this.props.email}
 					/>
 				</CardSection>
 
@@ -25,6 +30,8 @@ class LoginForm extends Component {
 						secureTextEntry
 						label={'Password'}
 						placeholder={'password'}
+						onChangeText={this.onPasswordChange.bind(this)}
+						value={this.props.password}
 					/>
 				</CardSection>
 
@@ -39,4 +46,11 @@ class LoginForm extends Component {
 	}
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = state => {
+	return  {
+		email: state.auth.email,
+		password: state.auth.password
+	};
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
