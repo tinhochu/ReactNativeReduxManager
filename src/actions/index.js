48107f2adc9e2d94bf9811351e374b73d102a6f7
Tin Chu <mchu.tinho@gmail.com>
2017-11-20 12:33:03 -0600

    spinner on the login form

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,9 @@ import firebase from 'firebase';
 import {
 	EMAIL_CHANGED,
 	PASSWORD_CHANGED,
-	LOGIN_USER_SUCCESS
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL,
+	LOGIN_USER
 } from './types';
 
 export const emailChanged = (text) => {
@@ -21,12 +23,28 @@ export const passwordChanged = text => {
 
 export const loginUser = ({ email, password }) => {
 	return (dispatch) => {
+		dispatch({ type: LOGIN_USER });
+
 		firebase.auth().signInWithEmailAndPassword(email, password)
-			.then(user => {
-				dispatch({
-					type: LOGIN_USER_SUCCESS,
-					payload: user
-				});
+			.then(user => loginUserSuccess(dispatch, user))
+			.catch((e) => {
+				console.log(e);
+
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(user => loginUserSuccess(dispatch, user))
+					.catch(error => loginUserFail(dispatch, error));
 			});
 	};
+};
+
+const loginUserFail = (dispatch, error) => {
+	console.log(error);
+	dispatch({ type: LOGIN_USER_FAIL, payload: error.message });
+}
+
+const loginUserSuccess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	});
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
@@ -16,6 +17,29 @@ class LoginForm extends Component {
 		const { email, password } = this.props;
 
 		this.props.loginUser({ email, password });
+	}
+
+	renderError() {
+		if (this.props.error) {
+			return (
+				<View style={{ backgroundColor: 'white' }}>
+					<Text style={styles.errorTextStyle}>
+						{this.props.error}
+					</Text>
+				</View>
+			);
+		}
+	}
+
+	renderButton(){
+		if (this.props.loading) {
+			return <Spinner size={'large'} />
+		}
+		return (
+			<Button onPress={this.onButtonPress.bind(this)}>
+				Login
+			</Button>
+		);
 	}
 
 	render() {
@@ -41,10 +65,10 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				{this.renderError()}
+
 				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>
-						Login
-					</Button>
+					{this.renderButton()}
 				</CardSection>
 
 			</Card>
@@ -52,10 +76,21 @@ class LoginForm extends Component {
 	}
 }
 
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
+
 const mapStateToProps = state => {
-	return  {
-		email: state.auth.email,
-		password: state.auth.password
+	const { email, password, error, loading } = state.auth;
+	return {
+		email,
+		password,
+		error,
+		loading
 	};
 };
 
