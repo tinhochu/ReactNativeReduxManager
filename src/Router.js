52ea6b3dcbef96af8c866dfa19777ed85c926bea
Tin Chu <mchu.tinho@gmail.com>
2017-11-21 14:45:07 -0600

    employeeReducer

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
-
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
 	return (
@@ -14,7 +14,19 @@ const RouterComponent = () => {
 				</Scene>
 
 				<Scene key="main">
-					<Scene key="employeeList" component={EmployeeList} title="Employees" />
+					<Scene
+						onRight={() => Actions.employeeCreate()}
+						rightTitle={'Add'}
+						key="employeeList"
+						component={EmployeeList}
+						title="Employees"
+						initial
+					/>
+					<Scene
+						key="employeeCreate"
+						component={EmployeeCreate}
+						title="Create Employee"
+					/>
 				</Scene>
 
 			</Scene>
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Card, CardSection, Input, Button, Spinner } from './common';
+import { employeeUpdate } from '../actions';
+
+class EmployeeCreate extends Component {
+
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<Input
+						label={'Name'}
+						placeholder={'Jane'}
+						value={this.props.name}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+					/>
+				</CardSection>
+				<CardSection>
+					<Input
+						label={'Phone'}
+						placeholder={'555-555-5555'}
+						value={this.props.phone}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+					/>
+				</CardSection>
+
+				<CardSection>
+
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Create
+					</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	const { name, phone, shift } = state.employeeForm;
+	return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
