24c032b0b84b78a94fe80490c3dffef3fe9b9572
Tin Chu <mchu.tinho@gmail.com>
2017-11-22 12:58:41 -0600

    Apply for the Reducer

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
 	EMPLOYEE_UPDATE
 } from './types';
@@ -6,5 +8,18 @@ export const employeeUpdate = ({ prop, value }) => {
 	return {
 		type: EMPLOYEE_UPDATE,
 		payload: { prop, value }
+	};
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+	const { currentUser } = firebase.auth();
+
+	return () => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			.push({ name, phone, shift })
+			.then(() => {
+				Actions.pop();
+				// Actions.employeeList({ type: 'reset' }); // reset the entire view stack
+			});
 	};
 };
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button, Spinner } from './common';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 
 class EmployeeCreate extends Component {
+	onButtonPress() {
+		const { name, phone, shift } = this.props;
+		this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+	}
 
 	render() {
 		return (
@@ -44,7 +48,7 @@ class EmployeeCreate extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Create
 					</Button>
 				</CardSection>
@@ -65,4 +69,7 @@ const mapStateToProps = state => {
 	return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+	employeeUpdate,
+	employeeCreate
+})(EmployeeCreate);
