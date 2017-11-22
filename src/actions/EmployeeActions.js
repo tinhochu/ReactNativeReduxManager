c86331bcae63d09bc84daa5fe8e60972a4973e51
Tin Chu <mchu.tinho@gmail.com>
2017-11-22 13:19:19 -0600

    fetching data

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
-	EMPLOYEE_UPDATE
+	EMPLOYEE_UPDATE,
+	EMPLOYEE_CREATE,
+	EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -14,12 +16,24 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
 	const { currentUser } = firebase.auth();
 
-	return () => {
+	return (dispatch) => {
 		firebase.database().ref(`/users/${currentUser.uid}/employees`)
 			.push({ name, phone, shift })
 			.then(() => {
+				dispatch({
+					type: EMPLOYEE_CREATE
+				});
 				Actions.pop();
-				// Actions.employeeList({ type: 'reset' }); // reset the entire view stack
+			});
+	};
+};
+
+export const employeesFetch = () => {
+	return (dispatch) => {
+		const { currentUser } = firebase.auth();
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			.on('value', snapshot => {
+				dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
 			});
 	};
 };
