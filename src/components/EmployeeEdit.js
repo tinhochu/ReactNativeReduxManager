a171c2d65cc2d112d1d1bb3ad486e9bb4fdcb1fd
Tin Chu <mchu.tinho@gmail.com>
2017-11-27 10:54:35 -0600

    edit employees done

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,22 +1,31 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Button } from './common';
-import { employeeUpdate, employeeCreate } from '../actions';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate, employeeSave } from '../actions';
 
-class EmployeeCreate extends Component {
+
+class EmployeeEdit extends Component {
+	componentWillMount() {
+		_.each(this.props.employee, (value, prop) => {
+			this.props.employeeUpdate({ prop, value });
+		});
+	}
+
 	onButtonPress() {
 		const { name, phone, shift } = this.props;
-		this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+		this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
 	}
 
 	render() {
 		return (
 			<Card>
-				<EmployeeForm {...this.props} />
+				<EmployeeForm />
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
-						Create
+						Save Changes
 					</Button>
 				</CardSection>
 			</Card>
@@ -26,10 +35,7 @@ class EmployeeCreate extends Component {
 
 const mapStateToProps = state => {
 	const { name, phone, shift } = state.employeeForm;
-	return { name, phone, shift };
+	return {	name, phone, shift };
 };
 
-export default connect(mapStateToProps, {
-	employeeUpdate,
-	employeeCreate
-})(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
